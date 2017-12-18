fa9858a8e3da18e0d0c79e5a42b7310640dbc21a
napalm255 <napalm255@gmail.com>
2017-12-18 17:35:00 -0500

    testing plotly

--- index.py
+++ index.py
@@ -1,12 +1,26 @@
-import json
-import datetime
+#!/usr/bin/env python
+"""Commute Traffic Data."""
+
+import pymysql
+from plotly import __version__
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+from plotly.graph_objs import Scatter, Figure, Layout
+
 
 
 def handler(event, context):
-    data = {
-        'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
-    }
+    """Lambda handler."""
+    # pylint: disable=unused-argument
+
+    plot([Scatter(x=[1, 2, 3], y=[3, 1, 6])], auto_open=False)
+
+    with open('temp-plot.html') as html_file:
+        html_contents = html_file.read()
+
     return {'statusCode': 200,
-            'body': json.dumps(data),
-            'headers': {'Content-Type': 'application/json'}}
+            'body': html_contents,
+            'headers': {'Content-Type': 'text/html'}}
+
+
+if __name__ == '__main__':
+    print(handler({}, None))
