ccde8e9eac9c88c9ba33a193e16d0773cb56a70b
napalm255 <napalm255@gmail.com>
2017-12-18 21:17:39 -0500

    output database records

--- index.py
+++ index.py
@@ -1,25 +1,50 @@
 #!/usr/bin/env python
 """Commute Traffic Data."""
 
+from __future__ import print_function
+import sys
+import logging
+import os
+import json
+from collections import OrderedDict
 import pymysql
-from plotly import __version__
-from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
-from plotly.graph_objs import Scatter, Figure, Layout
 
+
+try:
+    DATA = {'db_host': os.environ['DATABASE_HOST'],
+            'db_user': os.environ['DATABASE_USER'],
+            'db_pass': os.environ['DATABASE_PASS'],
+            'db_name': os.environ['DATABASE_NAME']}
+    CONNECTION = pymysql.connect(host=DATA['db_host'],
+                                 user=DATA['db_user'],
+                                 password=DATA['db_pass'])
+    logging.info('Successfully connected to MySql.')
+except:
+    logging.error('Unexpected error: could not connect to MySql.')
+    sys.exit()
 
 
 def handler(event, context):
     """Lambda handler."""
-    # pylint: disable=unused-argument
+    # pylint: disable=unused-argument, too-many-locals
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    logger.info(event)
 
-    plot([Scatter(x=[1, 2, 3], y=[3, 1, 6])], auto_open=False)
+    header = {'Content-Type': 'application/json'}
 
-    with open('temp-plot.html') as html_file:
-        html_contents = html_file.read()
+    table_name = 'traffic'
+
+    with CONNECTION.cursor() as cursor:
+        # check if database exists
+        cursor.execute('select * from %s' % (table_name))
+        recs = cursor.fetchall()
+        logging.info(recs)
 
     return {'statusCode': 200,
-            'body': html_contents,
-            'headers': {'Content-Type': 'text/html'}}
+            'body': json.dumps({'status': 'OK',
+                                'records': recs}),
+            'headers': header}
 
 
 if __name__ == '__main__':
